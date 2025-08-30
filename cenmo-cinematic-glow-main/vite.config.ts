2ca827201c32e51a9a25439f6becde213f1311fd
Ritik77620 <177842599+Ritik77620@users.noreply.github.com>
2025-08-30 22:53:07 +0530

    Update vite.config.ts

--- cenmo-cinematic-glow-main/vite.config.ts
+++ cenmo-cinematic-glow-main/vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/<your-repo-name>/" : "/", // 👈 add this line
+  base: mode === "production" ? "/<your-repo-name>/" : "/", // 👈 IMPORTANT
   server: {
     host: "::",
     port: 8080,
